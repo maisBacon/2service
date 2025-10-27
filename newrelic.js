d491e444dab1f2210e47f3793af7d9bc9c50c235
Melo, Renan <Renan.Melo@AB-Inbev.com>
2025-10-27 17:59:55 -0300

    .

--- newrelic.js
+++ newrelic.js
@@ -1,12 +1,20 @@
 'use strict';
+
+const { application } = require("express");
+
 module.exports = {
-  app_name: [process.env.NEW_RELIC_APP_NAME || 'service-2'],
+  app_name: [process.env.NEW_RELIC_APP_NAME],
   license_key: process.env.NEW_RELIC_LICENSE_KEY,
   logging: {
     level: process.env.NEW_RELIC_LOG_LEVEL || 'info'
   },
   distributed_tracing: {
-    enabled: process.env.NEW_RELIC_DISTRIBUTED_TRACING_ENABLED === 'true' || true
+    enabled: process.env.NEW_RELIC_DISTRIBUTED_TRACING_ENABLED === 'true'
+  },
+  application_logging: {
+    enabled: true,
+    forwarding: {
+      enabled: true
+    }
   }
 };
-
